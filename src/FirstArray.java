9c5aa3ebe754e1edca0c296088932128a1a02611
Nicu Ceban <nikulet01@gmail.com>
2021-05-31 19:21:24 +0300

    first test

---
+++ src/FirstArray.java
@@ -0,0 +1,8 @@
+public class FirstArray {
+    public static void main(String[] args) {
+        int[] firstArray = new int[]{90, 80, 70, 60, 50, 40, 30, 20, 10};
+        for (int i = 0, j = 8; i < j; j--) {
+            System.out.print(firstArray[j] + " ");
+        }
+    }
+}
---
+++ src/SecondArray.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class SecondArray {
+    public static void main(String[] args) {
+        int[] firstArray = new int[]{3, 1, 2, 9, 5, 4, 7, 6, 8};
+        Arrays.sort(firstArray);
+        System.out.println(Arrays.toString(firstArray));
+    }
+}
